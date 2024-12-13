bc3c19e9997ea6e135530ac5f3ade4bcfa0e7eb7
Arpanmukherjee3110 <arpanmukherjee3110@gmail.com>
2024-12-13 20:33:16 +0530

    update

---
+++ script/slide.js
@@ -0,0 +1,26 @@
+new Swiper('.cardwrapper', {
+    loop: true,
+    spaceBetween:30,
+  
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true
+    },
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        0:{
+            slidesPerView:1
+        },
+        768:{
+            slidesPerView:3
+        },
+        1024:{
+            slidesPerView:4
+        },
+    }
+  });
