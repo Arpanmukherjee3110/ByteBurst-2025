a4b312da98b4f7bc097f7daf2c3d5f9fcaca422f
Arpanmukherjee3110 <arpanmukherjee3110@gmail.com>
2024-12-13 15:36:00 +0530

    update

---
+++ script/type_anim.js
@@ -0,0 +1,12 @@
+
+var typed = new Typed('#typone', {
+    strings: ['learn to code','explore tech', 'have fun'],
+    typeSpeed: 100,
+    loop: true,
+  });
+ 
+  var typed = new Typed('#typtwo', {
+    strings: ['learn to code','explore tech', 'have fun'],
+    typeSpeed: 100,
+    loop: true,
+  });
